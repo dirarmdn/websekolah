696e6687963944152eb15ddd9c30140ab690ef43
dirarmdn <dhiraramadini@gmail.com>
2022-05-11 17:48:02 +0700

    edot

--- assets/script.js
+++ assets/script.js
@@ -5,14 +5,6 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
-$("#inpt_search").on('focus', function () {
-	$(this).parent('label').addClass('active');
-});
-
-$("#inpt_search").on('blur', function () {
-	if($(this).val().length == 0)
-		$(this).parent('label').removeClass('active');
-});
 $(function () {
   $(document).scroll(function () {
 	  var $nav = $(".navbar");
